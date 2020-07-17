f18ed25979cbb773c5a32c25daea00d83624fa16
Laurel Kelly <laurellkelly@gmail.com>
2020-07-17 16:13:27 -0700

    Commit all the files

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 // Write your JavaScript here
+
+var dollars, quarters, dimes, nickels, pennies;
+
+function calculateChange() {
+  var amountDue = document.getElementById('amount-due').value;
+  var amountReceived = document.getElementById('amount-received').value;
+  var change = amountReceived - amountDue;
+  //document.getElementById('change-output').innerHTML = change.toFixed(2);
+
+  var dollars = Math.floor(change);
+  document.getElementById("dollars-output").innerHTML = dollars;
+  var remainder = (change - dollars) * 100;
+
+  var quarters = Math.floor(remainder / 25);
+  document.getElementById("quarters-output").innerHTML = quarters;
+  remainder = remainder - (quarters * 25);
+
+  var dimes = Math.floor(remainder / 10);
+  document.getElementById("dimes-output").innerHTML = dimes;
+  remainder = remainder - (dimes * 10);
+
+  var nickels = Math.floor(remainder / 5);
+  document.getElementById("nickels-output").innerHTML = nickels;
+  remainder = remainder - (nickels * 5);
+
+  var pennies = remainder;
+  document.getElementById("pennies-output").innerHTML = pennies;
+
+};
+
+document.getElementById("calculate-change").addEventListener("click", calculateChange);
